cff5d4693f181a6b38dfd15bd15d9a8ae85bb2dd
Tom Dugan <thomas@aadwebb.com>
2019-09-13 12:18:22 -0500

    wip

---
+++ src/initial-state.js
@@ -0,0 +1,58 @@
+const initialState = {
+  currentStep: 'ClassSelect',
+  currentSlot: 0,
+  party: {
+    '0': {
+      name: '',
+      bio: '',
+      klass: null,
+      attributes: {
+        str: null,
+        int: null,
+        wis: null,
+        dex: null,
+        con: null,
+        cha: null,
+      },
+    },
+    '1': {
+      name: '',
+      bio: '',
+      klass: null,
+      attributes: {
+        str: null,
+        int: null,
+        wis: null,
+        dex: null,
+        con: null,
+        cha: null,
+      },
+    },
+    '2': {
+      name: '',
+      bio: '',
+      klass: null,
+      attributes: {
+        str: null,
+        int: null,
+        wis: null,
+        dex: null,
+        con: null,
+        cha: null,
+      },
+    },
+    '3': {
+      name: '',
+      bio: '',
+      klass: null,
+      attributes: {
+        str: null,
+        int: null,
+        wis: null,
+        dex: null,
+        con: null,
+        cha: null,
+      },
+    },
+  }
+}
